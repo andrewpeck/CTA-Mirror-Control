351f95b8f0276ca0087870e1cfbd94968363d9b8
Andrew Peck <anp@ucla.edu>
2014-05-20 20:16:04 -0700

    Misc changes, addition of destructor

--- MirrorControlBoard.hpp
+++ MirrorControlBoard.hpp
@@ -33,16 +33,19 @@ class MirrorControlBoard
 {
 public:
 
+  MirrorControlBoard(Sys* sys, bool no_initialize = false, unsigned nusb = 7, unsigned ssp_clk_div = 2);
+  ~MirrorControlBoard(); 
+
   enum UStep { USTEP_1, USTEP_2, USTEP_4, USTEP_8 };
   enum Dir { DIR_EXTEND, DIR_RETRACT, DIR_NONE };
-
-  MirrorControlBoard(Sys* sys, bool no_initialize = false,
-		     unsigned nusb = 7, unsigned ssp_clk_div = 2);
+  enum GPIODir { DIR_OUTPUT, DIR_INPUT};
   
   void initialize(const unsigned ssp_clk_div);
-  void initializeSSP(const unsigned ssp_clk_div);
+  void initializeSPI();
 
-  // Power control
+  ////////////////////////////////////////////////////////////////////////////////
+  // Power Control Function Prototypes
+  ////////////////////////////////////////////////////////////////////////////////
   void powerDownAll();
   void powerUpAll();
 
@@ -67,7 +70,9 @@ public:
   void powerDownADCs();
   bool isADCsPoweredUp() const;
 
-  // Drives
+  ////////////////////////////////////////////////////////////////////////////////
+  // Motor Driver Function Prototypes
+  ////////////////////////////////////////////////////////////////////////////////
   void stepOneDrive(unsigned idrive, Dir dir, unsigned ndelayloop = 3000);
   void stepAllDrives(Dir dr1_dir, Dir dr2_dir, Dir dr3_dir,
 		     Dir dr4_dir, Dir dr5_dir, Dir dr6_dir,
@@ -92,38 +97,26 @@ public:
   void disableDriveHiCurrent() { enableDriveHiCurrent(false); }
   bool isDriveHiCurrentEnabled() const;
   
-
-  // ADCs
+  ////////////////////////////////////////////////////////////////////////////////
+  // ADC Function Prototypes
+  ////////////////////////////////////////////////////////////////////////////////
   void initializeADC(unsigned iadc);
   void selectADC(unsigned iadc);
-  uint32_t measureADC(unsigned iadc, unsigned ichan, 
-		      unsigned ndelayloop = 100);
-  void measureManyADC(uint32_t* data, 
-		      unsigned iadc, unsigned zchan, unsigned nchan, 
-		      unsigned ndelayloop);
-  uint32_t measureADCWithBurn(unsigned iadc, unsigned ichan, 
-			      unsigned ndelayloop = 100);
-  void measureManyADCWithBurn(uint32_t* data, 
-			      unsigned iadc, unsigned zchan, unsigned nchan, 
-			      unsigned ndelayloop);
-  void measureADCStat(unsigned iadc, unsigned ichan, unsigned nmeas, 
-		      uint32_t& sum, uint64_t& sumsq, 
-		      uint32_t& min, uint32_t& max,
-		      unsigned nburn = 0, unsigned ndelayloop = 100);
-  void measureADC(unsigned iadc, unsigned ichan, unsigned nmeas, 
-              std::vector<uint32_t>& vMeas,
-		      unsigned ndelayloop = 100);
+  uint32_t measureADC(unsigned iadc, unsigned ichan, unsigned ndelayloop = 100);
+  void measureManyADC(uint32_t* data, unsigned iadc, unsigned zchan, unsigned nchan, unsigned ndelayloop);
+  uint32_t measureADCWithBurn(unsigned iadc, unsigned ichan, unsigned ndelayloop = 100);
+  void measureManyADCWithBurn(uint32_t* data, unsigned iadc, unsigned zchan, unsigned nchan, unsigned ndelayloop);
+  void measureADCStat(unsigned iadc, unsigned ichan, unsigned nmeas, uint32_t& sum, uint64_t& sumsq, uint32_t& min, uint32_t& max, unsigned nburn = 0, unsigned ndelayloop = 100);
+  void measureADC(unsigned iadc, unsigned ichan, unsigned nmeas, std::vector<uint32_t>& vMeas, unsigned ndelayloop = 100);
 
-  int measureEncoder(unsigned ichan, 
-		     unsigned calib_lo, unsigned calib_hi,
-		     unsigned ticks_per_rev = 400*8, const int* correction = 0)
-  {
+  int measureEncoder(unsigned ichan, unsigned calib_lo, unsigned calib_hi, unsigned ticks_per_rev = 400*8, const int* correction = 0) {
     int meas = int(measureADCWithBurn(7, ichan));
     int calib_range = calib_lo - calib_hi;
     meas -= calib_hi;
     meas *= ticks_per_rev;
     meas /= calib_range;
-    if(correction && meas>=0 && meas<int(ticks_per_rev))meas+=correction[meas];
+    if (correction && meas>=0 && meas<int(ticks_per_rev))
+        meas+=correction[meas];
     return meas;
   }
 
